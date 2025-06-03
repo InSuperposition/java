0753e39aaea8cba9b4884ca3584c5d18f2dbf7f0
InSuperposition <f.galloway@gmail.com>
2025-06-03 10:03:56 +0300

    ! deprication ! remove ENV variables

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-export GRAALVM_VERSION=graalvm-ce-java17-22.3.1
-export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/$GRAALVM_VERSION/Contents/Home
-export JAVA_HOME=$GRAALVM_HOME
-export JAVA_BIN_PATH=$JAVA_HOME/bin
+
+# export GRAALVM_VERSION=graalvm-ce-java17-22.3.1
+# export GRAALVM_HOME=/Library/Java/JavaVirtualMachines/$GRAALVM_VERSION/Contents/Home
+# export JAVA_HOME=$GRAALVM_HOME
+# export JAVA_BIN_PATH=$JAVA_HOME/bin
